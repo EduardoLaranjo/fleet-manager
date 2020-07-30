5e5aea220aacda489e73b54da4ec992f27bdca69
EduardoLaranjo <eduardolaranjo@gmail.com>
2020-07-30 11:02:38 +0100

    alert message

--- client/src/components/fleet/FleetMap.js
+++ client/src/components/fleet/FleetMap.js
@@ -1,25 +1,47 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import {GoogleMap} from "./GoogleMap";
-import {PointOfInterest, TruckPath} from "./Markers";
 import TruckApi from "../../apis/truck";
-
 import {SearchBar} from "./SearchBar";
 import {Distance} from "./Distance";
+import {TruckPath} from "./TruckPath";
+import {PointOfInterest} from "./PointsOfInterest";
+import {Message, Portal} from "semantic-ui-react";
+import "./FleetMap.css"
+
 
 export function FleetMap() {
 
   const [destination, setDestination] = useState(null)
-
   const [truck, setTruck] = useState({})
   const [searchPOI, setSearchPOI] = useState({radius: 0, poi: []})
+  const [err, setErr] = useState('')
+  const timeoutId = useRef(0)
 
   const handleOnSearch = async ({license, poi, radius}) => {
-    const {data: truck} = await TruckApi.get(license)
-    const currLocation = truck.path[truck.path.length - 1]
-
-    setTruck({currLocation: {...currLocation}, path: truck.path})
-    setSearchPOI( {radius: radius, poi: [...poi]})
     setDestination(null)
+
+    try {
+
+      clearTimeout(timeoutId.current)
+
+      const {data: truck} = await TruckApi.get(license)
+      const currLocation = truck.path[truck.path.length - 1]
+
+      setTruck({currLocation: {...currLocation}, path: truck.path})
+      setSearchPOI({radius: radius, poi: [...poi]})
+
+    } catch (e) {
+
+      if (e.response.status === 404) {
+        setErr(`Couldn't find truck with license ${license}.`)
+      } else {
+        setErr('Unexpected error contact support.')
+      }
+
+      timeoutId.current = setTimeout(() => setErr(''), 3000)
+
+    }
+
   };
 
   const renderTruckPath = () => {
@@ -43,7 +65,7 @@ export function FleetMap() {
 
   const renderDistance = () => {
     if (truck && destination) {
-      return <Distance from={truck.currLocation} to={destination} />
+      return <Distance from={truck.currLocation} to={destination}/>
     }
   };
 
@@ -57,7 +79,11 @@ export function FleetMap() {
         {renderDistance()}
         {renderPointsOfInterest()}
       </GoogleMap>
-
+      <Portal open={!!err}>
+        <Message className="message-error" compact negative size='small'>
+          <Message.Header>{err}</Message.Header>
+        </Message>
+      </Portal>
     </div>
   )
 }
